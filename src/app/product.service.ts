5a07ce2b1eb16d31609783253495bd7de5ce4b20
Franck,Zach T <ZFRANCK@travelers.com>
2020-04-14 12:41:40 -0500

    getting the response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 
 @Injectable()
-
-class _albumUrl{
-  static aUrl = "../assets/album.json";
-} 
-
 export class ProductService {
+  
+  private _albumUrl = '../assets/album.json'
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(_albumUrl.aUrl).subscribe(
-      Response.json()
-    )
+    return this._http.get(this._albumUrl).map((response) => response.json())
   }
 
 }
