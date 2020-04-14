7d31041e617f48cbe96ef5ba76b48b0fc1665096
Franck,Zach T <ZFRANCK@travelers.com>
2020-04-14 15:05:00 -0500

    casting part 2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album>{
-    return this._http.get(this._albumUrl).map((response) => response.json() as Album)
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
   }
 
 }
