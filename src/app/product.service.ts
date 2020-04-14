888e9ee5a8ed156ee55cae55033dc7c16843b8fb
Franck,Zach T <ZFRANCK@travelers.com>
2020-04-14 14:56:50 -0500

    4.1-7

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
@@ -10,7 +12,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number){
+  getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response) => response.json())
   }
 
