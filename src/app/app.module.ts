a40a5a153e99f351dce1ff5334bc0db9babbbe3a
Franck,Zach T <ZFRANCK@travelers.com>
2020-04-15 13:20:37 -0500

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { RouterModule, Routes } from '@angular/router'
 
 const appRoutes: Routes = [
   {path: 'products', component: ProductListComponent},
-  {path: 'products/:id', component: ProductPageComponent},
+  {path: 'product/:id', component: ProductPageComponent},
   {path: '', redirectTo:'products', pathMatch:'full'}
 ]
 @NgModule({
