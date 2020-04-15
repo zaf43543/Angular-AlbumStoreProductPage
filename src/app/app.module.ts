dde3baff8918bb73cba5cd30c74ea2cc34422896
Franck,Zach T <ZFRANCK@travelers.com>
2020-04-15 12:57:02 -0500

    part 7.1-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,12 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component'
+import { RouterModule, Routes } from '@angular/router'
 
+const appRoutes: Routes[
+  { path: 'products', component: ProductListComponent},
+  { path: 'products/:id', component: ProductPageComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
