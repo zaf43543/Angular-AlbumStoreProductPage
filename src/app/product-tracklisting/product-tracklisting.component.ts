de2ea6193f3641790b35d3fa3cb859fd2d14b22b
Franck,Zach T <ZFRANCK@travelers.com>
2020-04-15 07:48:01 -0500

    minor fix

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,12 +9,12 @@ import { Album } from '../album'
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  albuminfo: Album;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albuminfo = response)
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
